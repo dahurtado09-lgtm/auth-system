77680f82711fc17a9dfc0f925d6f30d5f154fa7a
Alejandro <da.hurtado09@ciaf.edu.co>
2025-11-05 21:22:41 -0500

    Actualizacion de clase 5 Noviembre

--- config/database.js
+++ config/database.js
@@ -2,35 +2,35 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-process.env.DB_NAME,
-process.env.DB_USER,
-process.env.DB_PASSWORD,
-{
-host: process.env.DB_HOST,
-port: process.env.DB_PORT,
-dialect: 'mysql',
-logging: false, // Cambiar a true para ver las consultas SQL
-pool: {
-max: 5,
-min: 0,
-acquire: 30000,
-idle: 10000
-}
-}
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false, // Cambiar a true para ver las consultas SQL
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
 );
 
 // Función para probar la conexión
 const testConnection = async () => {
-try {
-// await sequelize.sync({ force: true }); // para eliminar las tablas y crearlas de nuevo
-await sequelize.authenticate();
-console.log('Conexión a MySQL establecida correctamente');
-} catch (error) {
-console.error('Error al conectar con MySQL:', error);
-}
+  try {
+    // await sequelize.sync({ force: true }); // para eliminar las tablas y crearlas de nuevo
+    await sequelize.authenticate();
+    console.log('Conexión a MySQL establecida correctamente');
+  } catch (error) {
+    console.error('Error al conectar con MySQL:', error);
+  }
 };
 
 module.exports = {
-testConnection,
-sequelize
+  testConnection,
+  sequelize
 };
--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-require("dotenv").config();
-const express = require("express");
-const { corsMiddleware } = require("./shared/middleware/cors");
+console.log("Mi primera app en express.js");
+require('dotenv').config();
+const express = require('express');
+const { corsMiddleware } = require('./shared/middleware/cors');
 const { testConnection } = require('./config/database');
+const { syncModels } = require('./shared/models');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(corsMiddleware);
 
 // Inicializar base de datos
 const initializeDatabase = async () => {
-    await testConnection();
+  await testConnection();
+  await syncModels();
 };
 
-app.get('/', (req, res ) => {
-    res.json({
-        message: "!Hola Express funcionando con MySQL",
-        timestamp: new Date(). toISOString(),
-        status: 'succes'
-    });
+app.get('/', (req, res) => {
+  console.log(`Sistema de login funcionando correctamente en el puerto ${PORT}`);
+  res.json({
+    message: '¡Hola! Express funcionando con MySQL',
+    timestamp: new Date().toISOString(),
+    status: 'success'
+  });
 });
 
-//login
+// Login
 app.use('/api/v1', require('./routes/auth'));
 
-// inicializar el servidor 
-const starServer = async () => {
-    try {
-        await initializeDatabase();
-        app.listen(PORT, () => {
-            console.log(`Servidor en http://localhost:${PORT}`);
-        });
-} catch (error) {
+// Inicializar servidor
+const startServer = async () => {
+  try {
+    await initializeDatabase();
+    app.listen(PORT, () => {
+      console.log(`Servidor en http://localhost:${PORT}`);
+    });
+  } catch (error) {
     console.error('Error al iniciar el servidor:', error);
-}
+  }
 };
 
-starServer();
+startServer();
